5224f12e2500e609ecf1ea2dd1b920fc2fe16644
Joseph Ivie <joseph@lightningkite.com>
2024-07-11 09:49:14 -0600

    KiteUI client library separated

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(serverlibs.plugins.dokka) apply false
+    alias(serverlibs.plugins.kotlinJvm) apply false
+    alias(serverlibs.plugins.kotlinMultiplatform) apply false
+    alias(serverlibs.plugins.androidApp) apply false
+    alias(serverlibs.plugins.androidLibrary) apply false
+    alias(serverlibs.plugins.graalVmNative) apply false
+    alias(serverlibs.plugins.shadow) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(serverlibs.deployHelpers)
+        classpath(serverlibs.proguard)
+    }
+}
+
+allprojects {
+    group = "com.lightningkite.lightningserver"
+    repositories {
+        mavenLocal()
+//        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/releases/")
+        google()
+        mavenCentral()
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+rootProject.name = "lightning-server-kiteui"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kspVersion: String by settings
+
+    repositories {
+        mavenLocal()
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/releases/")
+    }
+
+    plugins {
+        kotlin("plugin.serialization") version kotlinVersion
+        id("com.google.devtools.ksp") version kspVersion
+    }
+
+    dependencyResolutionManagement {
+        repositories {
+            mavenLocal()
+            google()
+            gradlePluginPortal()
+            mavenCentral()
+            maven("https://jitpack.io")
+        }
+
+        versionCatalogs {
+            create("serverlibs") { from(files("gradle/serverlibs.versions.toml"))}
+        }
+    }
+}
+
+include(":client")
+
