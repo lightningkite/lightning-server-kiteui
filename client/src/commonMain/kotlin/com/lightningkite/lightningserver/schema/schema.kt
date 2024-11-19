02521934c51989b62fdc6355a0ca005a7415d2d5
Joseph Ivie <joseph@lightningkite.com>
2024-11-19 14:48:32 -0700

    Bulkifier and cache fix

--- client/src/commonMain/kotlin/com/lightningkite/lightningserver/schema/schema.kt
+++ client/src/commonMain/kotlin/com/lightningkite/lightningserver/schema/schema.kt
@@ -20,6 +20,8 @@ import com.lightningkite.serialization.*
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.properties.Properties
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 fun SerializationRegistry.register(schema: LightningServerKSchema) {
     schema.structures.values.forEach { register(it) }
@@ -31,17 +33,24 @@ private fun LightningServerKSchema.uploadEarlyEndpoint() = endpoints.find {
     it.output.serialName == "com.lightningkite.lightningserver.files.UploadInformation"
 }
 
-private fun LightningServerKSchema.bulkEndpoint() = endpoints.find {
-    it.path.contains("bulk") &&
-            it.input.serialName == "com.lightningkite.lightningserver.typed.BulkRequest" &&
-            it.output.serialName == "com.lightningkite.lightningserver.typed.BulkResponse"
+private fun LightningServerKSchema.bulkEndpoint(): LightningServerKSchemaEndpoint? {
+    println("looking for bulk")
+    return endpoints.find {
+        it.path.contains("bulk") &&
+                it.input.serialName == "kotlin.collections.LinkedHashMap" &&
+                it.input.arguments.getOrNull(0)?.serialName == "kotlin.String" &&
+                it.input.arguments.getOrNull(1)?.serialName == "com.lightningkite.lightningserver.typed.BulkRequest" &&
+                it.output.serialName == "kotlin.collections.LinkedHashMap" &&
+                it.output.arguments.getOrNull(0)?.serialName == "kotlin.String" &&
+                it.output.arguments.getOrNull(1)?.serialName == "com.lightningkite.lightningserver.typed.BulkResponse"
+    }
 }
 
 class ExternalLightningServer(
     val schema: LightningServerKSchema,
     val registry: SerializationRegistry = SerializationRegistry.master.copy(),
     val json: Json = DefaultJson,
-    val properties: Properties = UrlProperties
+    val properties: Properties = UrlProperties,
 ) {
     init {
         registry.register(schema)
@@ -119,7 +128,7 @@ class ExternalLightningServer(
         },
     )
 
-    inner class ModelInfo<T: HasId<ID>, ID: Comparable<ID>>(val inter: LightningServerKSchemaInterface){
+    inner class ModelInfo<T : HasId<ID>, ID : Comparable<ID>>(val inter: LightningServerKSchemaInterface) {
         val serializer = inter.matches.arguments[0].serializer(registry, mapOf()) as KSerializer<T>
         val idserializer = serializer.serializableProperties!!.find { it.name == "_id" }!!.serializer as KSerializer<ID>
         val vserializer = inter.matches.arguments[0].serializer(registry, mapOf()) as KSerializer<HasId<Comparable<Comparable<*>>>>
@@ -130,60 +139,66 @@ class ExternalLightningServer(
         val hasUpdatesWs =
             schema.endpoints.any { it.path == inter.path && it.method == "WEBSOCKET" && it.input.serialName == "com.lightningkite.lightningdb.Condition" && it.output.serialName == "com.lightningkite.lightningdb.CollectionUpdates" }
 
+        private var cacheCache = PerAuthCache { auth ->
+            println("Cache ${serializer.descriptor.serialName} created for ${auth?.sessionToken?.take(15)}")
+            when {
+                hasUpdatesWs -> ClientModelRestEndpointsPlusUpdatesWebsocketStandardImpl(
+                    fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
+                    wsImplementation = {
+                        multiplexSocket(
+                            url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
+                            path = inter.path,
+                            params = emptyMap(),
+                            json = json,
+                            pingTime = 5_000
+                        )
+                    },
+                    serializer = serializer,
+                    idSerializer = idserializer,
+                    json = json,
+                    properties = properties
+                )
+
+                hasWs -> ClientModelRestEndpointsPlusWsStandardImpl(
+                    fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
+                    wsImplementation = {
+                        multiplexSocket(
+                            url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
+                            path = inter.path,
+                            params = emptyMap(),
+                            json = json,
+                            pingTime = 5_000
+                        )
+                    },
+                    serializer = serializer,
+                    idSerializer = idserializer,
+                    json = json,
+                    properties = properties
+                )
+
+                else -> ClientModelRestEndpointsStandardImpl(
+                    fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
+                    wsImplementation = {
+                        multiplexSocket(
+                            url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
+                            path = inter.path,
+                            params = emptyMap(),
+                            json = json,
+                            pingTime = 5_000
+                        )
+                    },
+                    serializer = serializer,
+                    idSerializer = idserializer,
+                    json = json,
+                    properties = properties
+                )
+            }.let { ModelCache(it, it.serializer) } as ModelCache<T, ID>
+        }
+
         @Suppress("UNCHECKED_CAST")
-        fun cache(auth: LightningServerAuthentication?) = when {
-            hasUpdatesWs -> ClientModelRestEndpointsPlusUpdatesWebsocketStandardImpl(
-                fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
-                wsImplementation = {
-                    multiplexSocket(
-                        url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
-                        path = inter.path,
-                        params = emptyMap(),
-                        json = json,
-                        pingTime = 5_000
-                    )
-                },
-                serializer = serializer,
-                idSerializer = idserializer,
-                json = json,
-                properties = properties
-            )
-
-            hasWs -> ClientModelRestEndpointsPlusWsStandardImpl(
-                fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
-                wsImplementation = {
-                    multiplexSocket(
-                        url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
-                        path = inter.path,
-                        params = emptyMap(),
-                        json = json,
-                        pingTime = 5_000
-                    )
-                },
-                serializer = serializer,
-                idSerializer = idserializer,
-                json = json,
-                properties = properties
-            )
-
-            else -> ClientModelRestEndpointsStandardImpl(
-                fetchImplementation = auth?.let { fetcher(httpPath, it) } ?: authlessFetcher(httpPath),
-                wsImplementation = {
-                    multiplexSocket(
-                        url = schema.baseWsUrl + "?path=multiplex" + (auth?.sessionToken?.let { "?jwt=$it" } ?: ""),
-                        path = inter.path,
-                        params = emptyMap(),
-                        json = json,
-                        pingTime = 5_000
-                    )
-                },
-                serializer = serializer,
-                idSerializer = idserializer,
-                json = json,
-                properties = properties
-            )
-        }.let { ModelCache(it, it.serializer) } as ModelCache<T, ID>
+        fun cache(auth: LightningServerAuthentication?): ModelCache<T, ID> = cacheCache(auth)
     }
+
     val models: Map<String, ModelInfo<*, *>> = schema.interfaces.filter {
         it.matches.serialName == "ClientModelRestEndpoints"
     }.associate { inter ->
@@ -195,7 +210,7 @@ class ExternalLightningServer(
             { file ->
                 val req = authlessFetcher(schema.baseUrl).invoke(it.path, HttpMethod.GET, null, UploadInformation.serializer())
                 val r = connectivityFetch(req.uploadUrl, HttpMethod.PUT, body = RequestBodyFile(file))
-                if(!r.ok) throw IllegalStateException("File upload to ${req.uploadUrl.substringBefore('?')} failed")
+                if (!r.ok) throw IllegalStateException("File upload to ${req.uploadUrl.substringBefore('?')} failed")
                 ServerFile(req.futureCallToken)
             }
         }
@@ -217,9 +232,27 @@ class ExternalLightningServer(
         }
     }
 
-    var screen: (type: ModelInfo<*, *>, id: Comparable<*>?) -> (() -> Screen)? = { _, _ -> null}
+    var screen: (type: ModelInfo<*, *>, id: Comparable<*>?) -> (() -> Screen)? = { _, _ -> null }
+
     init {
         println("Init Complete")
     }
 }
 
+class PerAuthCache<T>(val calculate: (LightningServerAuthentication?) -> T) {
+    val forNull = calculate(null)
+    var lastKnown: LightningServerAuthentication? = null
+    var lastValue: T? = null
+    operator fun invoke(auth: LightningServerAuthentication?): T {
+        if(auth == null) return forNull
+        val lastValue = lastValue
+        if (lastValue == null || auth != lastKnown) {
+            val v = calculate(auth)
+            this.lastKnown = auth
+            this.lastValue = v
+            return v
+        } else {
+            return lastValue
+        }
+    }
+}
