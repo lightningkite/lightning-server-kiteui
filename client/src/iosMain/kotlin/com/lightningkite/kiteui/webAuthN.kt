16ddbb5d80f9712c7b56f13e5b760dfec49b18f3
Brady Svedin <brady@lightningkite.com>
2025-05-01 11:39:23 -0600

    iOS webAuthN stub fix

--- client/src/iosMain/kotlin/com/lightningkite/kiteui/webAuthN.kt
+++ client/src/iosMain/kotlin/com/lightningkite/kiteui/webAuthN.kt
@@ -1,15 +1,16 @@
 package com.lightningkite.kiteui
 
-import com.lightningkite.readable.Constant
-import com.lightningkite.readable.Readable
-import com.lightningkite.lightningserver.auth.proof.AssertedPublicKeyCredential
-import com.lightningkite.lightningserver.auth.proof.AttestedPublicKeyCredential
-import com.lightningkite.lightningserver.auth.proof.PublicKeyCredentialCreationOptions
-import com.lightningkite.lightningserver.auth.proof.PublicKeyCredentialRequestOptions
+import com.lightningkite.lightningserver.auth.proof.WebAuthN
 
-actual object ClientAuthenticator {
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class ClientAuthenticator {
     actual suspend fun webAuthNAvailable(): Boolean = false
     actual suspend fun autofillAvailable(): Boolean = false
-    actual suspend fun createWebAuthNCredentials(request: PublicKeyCredentialCreationOptions): AttestedPublicKeyCredential = TODO()
-    actual suspend fun getWebAuthNCredentials(request: PublicKeyCredentialRequestOptions, mediation: WebAuthNMediationType): AssertedPublicKeyCredential = TODO()
+    actual suspend fun createWebAuthNCredentials(request: WebAuthN.Registration.PublicKeyCredentialCreationOptions): WebAuthN.Registration.AttestedPublicKeyCredential = TODO()
+    actual suspend fun getWebAuthNCredentials(request: WebAuthN.Authentication.PublicKeyCredentialRequestOptions, mediation: WebAuthNMediationType): WebAuthN.Authentication.AssertedPublicKeyCredential = TODO()
+
+    actual companion object {
+        actual fun getClientAuthenticator(): ClientAuthenticator = TODO()
+    }
 }
