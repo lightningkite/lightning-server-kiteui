8ebdcfac4d638eca42f40a4ac139ee2034576b69
Joseph Ivie <joseph@lightningkite.com>
2025-06-04 13:02:32 -0600

    Updated versions

--- client/src/commonMain/kotlin/com/lightningkite/lightningserver/db/CacheUpdate.kt
+++ client/src/commonMain/kotlin/com/lightningkite/lightningserver/db/CacheUpdate.kt
@@ -9,7 +9,7 @@ sealed class CacheUpdate<T : HasId<ID>, ID : Comparable<ID>> {
     abstract val items: Collection<T>?
     class SocketOverload<T : HasId<ID>, ID : Comparable<ID>>(): CacheUpdate<T, ID>() {
         override val items: Collection<T>? get() = null
-        override fun toString(): String = "SocketOverload"
+        override fun toString(): String = "SocketOverload($items)"
     }
     class SocketChanges<T : HasId<ID>, ID : Comparable<ID>>(
         val changed: Set<T>,
@@ -22,32 +22,32 @@ sealed class CacheUpdate<T : HasId<ID>, ID : Comparable<ID>> {
             val activatedAt: Instant?
         }
         override val items: Collection<T> get() = changed
-        override fun toString(): String = "SocketChanges"
+        override fun toString(): String = "SocketChanges($changed, $removed, $fromCondition, $fromRequirements)"
     }
     class QueryResult<T : HasId<ID>, ID : Comparable<ID>>(
         val query: Query<T>,
         val result: List<T>
     ): CacheUpdate<T, ID>(){
         override val items: Collection<T> get() = result
-        override fun toString(): String = "QueryResult"
+        override fun toString(): String = "QueryResult($query, $result)"
     }
     class MultiGetResult<T : HasId<ID>, ID : Comparable<ID>>(
         val missing: Set<ID>,
         val result: List<T>
     ): CacheUpdate<T, ID>(){
         override val items: Collection<T> get() = result
-        override fun toString(): String = "MultiGetResult"
+        override fun toString(): String = "MultiGetResult($missing, $result)"
     }
     class MutationResult<T : HasId<ID>, ID : Comparable<ID>>(
         override val items: Collection<T>
     ): CacheUpdate<T, ID>() {
 
-        override fun toString(): String = "MutationResult"
+        override fun toString(): String = "MutationResult($items)"
     }
     class DeletionResult<T : HasId<ID>, ID : Comparable<ID>>(
         val deletedIds: Set<ID>
     ): CacheUpdate<T, ID>() {
         override val items: Collection<T> = listOf()
-        override fun toString(): String = "DeletionResult"
+        override fun toString(): String = "DeletionResult($deletedIds)"
     }
 }
