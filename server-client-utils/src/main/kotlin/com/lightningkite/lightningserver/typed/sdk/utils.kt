8d9179a4c8e8d2abcfc15a971dfc725e3e23bf3a
iHoonter <huntdawghero@gmail.com>
2025-12-11 16:03:12 -0700

    version updates
    Signed-off-by: iHoonter <huntdawghero@gmail.com>

--- server-client-utils/src/main/kotlin/com/lightningkite/lightningserver/typed/sdk/utils.kt
+++ server-client-utils/src/main/kotlin/com/lightningkite/lightningserver/typed/sdk/utils.kt
@@ -1,12 +1,14 @@
 package com.lightningkite.lightningserver.typed.sdk
 
 import com.lightningkite.lightningserver.runtime.ServerRuntime
+import com.lightningkite.services.data.ExperimentalLightningServer
 import com.lightningkite.services.data.KFile
 
+@OptIn(ExperimentalLightningServer::class)
 private class MergedFormat(val formats: List<SDK.Format>): SDK.Format {
     context(_: ServerRuntime)
-    override fun write(folder: KFile) =
-        formats.forEach { it.write(folder) }
+    override fun write(archive: Archive) =
+        formats.forEach { it.write(archive) }
 }
 
 public operator fun SDK.Format.plus(other: SDK.Format): SDK.Format = when {
